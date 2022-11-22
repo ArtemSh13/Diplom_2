3ea421a1f57c340fd2785ed1dc623f7558d66a75
ArtemSh13 <mozg1303@ya.ru>
2022-11-22 23:44:25 +0300

    Add tests of user update

---
+++ src/test/java/UpdateUserTest.java
@@ -0,0 +1,127 @@
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UpdateUserTest {
+    private User user;
+    private String userJson;
+    private final Gson gson = new Gson();
+    private String accessToken;
+    private String refreshToken;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = StellarBurgersAPI.BASE_URL;
+        user = new User(DataGenerator.getRandomEmail(), DataGenerator.getRandomPassword(), DataGenerator.getRandomName());
+        userJson = gson.toJson(user);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(userJson)
+                        .when()
+                        .post(StellarBurgersAPI.CREATE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(true))
+                .and().statusCode(200);
+
+        accessToken = response.then().extract().path("accessToken");
+        refreshToken = response.then().extract().path("refreshToken");
+
+    }
+
+    @Test
+    public void updateEmailOfSignedInUser() {
+        user.setEmail("updated_" + user.getEmail());
+        userJson = gson.toJson(user);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .auth().oauth2(accessToken.replace("Bearer ", ""))
+                        .body(userJson)
+                        .when()
+                        .patch(StellarBurgersAPI.UPDATE_AND_DELETE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(true))
+                .and().body("user.email", equalTo(user.getEmail()))
+                .and().body("user.name", equalTo(user.getName()))
+                .and().statusCode(200);
+
+    }
+
+    @Test
+    public void updateNameOfSignedInUser() {
+        user.setName("updated_" + user.getName());
+        userJson = gson.toJson(user);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .auth().oauth2(accessToken.replace("Bearer ", ""))
+                        .body(userJson)
+                        .when()
+                        .patch(StellarBurgersAPI.UPDATE_AND_DELETE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(true))
+                .and().body("user.email", equalTo(user.getEmail()))
+                .and().body("user.name", equalTo(user.getName()))
+                .and().statusCode(200);
+
+    }
+
+    @Test
+    public void updateEmailOfNotSignedInUser() {
+        user.setEmail("updated_" + user.getEmail());
+        userJson = gson.toJson(user);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .body(userJson)
+                        .when()
+                        .patch(StellarBurgersAPI.UPDATE_AND_DELETE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(false))
+                .and().body("message", equalTo(StellarBurgersAPI.expectedRequiredAuthorisedMessage))
+                .and().statusCode(401);
+
+    }
+
+    @Test
+    public void updateNameOfNotSignedInUser() {
+        user.setName("updated_" + user.getName());
+        userJson = gson.toJson(user);
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .body(userJson)
+                        .when()
+                        .patch(StellarBurgersAPI.UPDATE_AND_DELETE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(false))
+                .and().body("message", equalTo(StellarBurgersAPI.expectedRequiredAuthorisedMessage))
+                .and().statusCode(401);
+
+    }
+
+    @After
+    public void teardown() {
+        Response response =
+                given()
+                        .auth().oauth2(accessToken.replace("Bearer ", ""))
+                        .when()
+                        .delete(StellarBurgersAPI.UPDATE_AND_DELETE_USER_API);
+
+        response.then().assertThat().body("success", equalTo(true))
+                .and().body("message", equalTo(StellarBurgersAPI.expectedSuccessfulRemovedMessage))
+                .and().statusCode(202);
+    }
+}
