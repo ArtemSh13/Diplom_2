bf1a80654fb008aea3ff84b3b7c2b49abb893201
ArtemSh13 <mozg1303@ya.ru>
2022-11-23 23:10:11 +0300

    Complete POJO for order

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -22,4 +22,8 @@ public class Order {
     public int getNumberOfIngredients() {
         return ingredients.size();
     }
+
+    public void addIngredient(String ingredient) {
+        ingredients.add(ingredient);
+    }
 }
