3a6f28f1980fef548e50e235ea6a7597e63166a6
ArtemSh13 <mozg1303@ya.ru>
2022-11-23 23:08:28 +0300

    Add POJO for order

---
+++ src/main/java/Order.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Order {
+    private List<String> ingredients;
+
+    public Order(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getIngredientByIndex(int index) {
+        return ingredients.get(index);
+    }
+
+    public int getNumberOfIngredients() {
+        return ingredients.size();
+    }
+}
